43dcdf713e876a2bd5a0f1025ba3eb5a1f3a80a3
RenTianle <lele.rer@outlook.com>
2022-07-23 14:52:10 +0800

    Initial setup for DHT11 sensor

---
+++ Humidity&Temp_Sensor/dht11.ino
@@ -0,0 +1,36 @@
+#include <dht.h>
+
+
+#define dht_apin A0 // Analog Pin sensor is connected to
+#define fan_pin A1 //Fan pin is connected to 
+dht DHT;
+
+void setup(){
+  Serial.begin(9600);
+  delay(500);
+  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  delay(1000);
+  pinMode(fan_pin, OUTPUT);
+  digitalWrite(fan_pin,LOW);
+}
+ 
+void loop(){
+ 
+    DHT.read11(dht_apin);
+    
+    Serial.print("Current humidity = ");
+    Serial.print(DHT.humidity);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(DHT.temperature); 
+    Serial.println("C  ");
+    
+    delay(5000);//Wait 5 seconds before accessing sensor again.
+ 
+  //Fastest should be once every two seconds.
+
+    if(DHT.temperature > 20) {
+      digitalWrite(fan_pin, HIGH);
+    }
+ 
+}
